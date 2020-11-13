287b70bf74d0e73fad293cb01747a6cec6a0b255
ferbel2626 <ff2604@hotmail.com>
2020-11-12 23:44:54 -0300

    Segunda versao

--- src/teste_git/Teste_GIT.java
+++ src/teste_git/Teste_GIT.java
@@ -16,6 +16,7 @@ public class Teste_GIT {
      */
     public static void main(String[] args) {
         System.out.println("Inicio do teste");
+        System.out.println("segunda  versão");
     }
     
 }
